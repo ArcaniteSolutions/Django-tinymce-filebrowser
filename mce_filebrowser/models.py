ce9db0084365f1fdbface5fe854bd642621b2a4f
Julien Eberle <julien.eberle@arcanite.ch>
2018-05-31 15:37:58 +0200

    Fix issues with special chars in uploaded files' name

--- mce_filebrowser/models.py
+++ mce_filebrowser/models.py
@@ -1,10 +1,14 @@
 import datetime
+import uuid
 from django.db import models
 from django.utils.translation import ugettext as _
 
 
 def content_file_name(instance, filename):
-    return "uploaded/{}/{}".format(datetime.datetime.now().strftime("%Y/%m/%d"), filename)
+    return "uploaded/{}/{}.{}".format(
+        datetime.datetime.now().strftime("%Y/%m/%d"),
+        uuid.uuid4(), filename.rpartition(".")[2]
+    )
 
 
 class FileBrowserFile(models.Model):
@@ -15,7 +19,7 @@ class FileBrowserFile(models.Model):
     )
 
     file_type = models.CharField(max_length=3, choices=FILE_TYPES)
-
+    file_name = models.CharField(max_length=255)
     uploaded_file = models.FileField(
         upload_to=content_file_name,
         verbose_name=_('File / Image'),
@@ -28,4 +32,4 @@ class FileBrowserFile(models.Model):
     user_id = models.IntegerField(null=True, blank=True, verbose_name=_('Who does this file belong to?'))
 
     def __str__(self):
-        return self.uploaded_file.name
+        return self.file_name
