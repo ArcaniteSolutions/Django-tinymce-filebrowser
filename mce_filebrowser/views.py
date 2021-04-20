e6d666c8eb3bf3c139460aa0c3946d5a2c5edc14
Julien Eberle <julien.eberle@arcanite.ch>
2021-04-20 21:56:44 +0200

    Fix django 2.2 compatibility

--- mce_filebrowser/views.py
+++ mce_filebrowser/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.text import slugify
 
 from mce_filebrowser.models import FileBrowserFile
